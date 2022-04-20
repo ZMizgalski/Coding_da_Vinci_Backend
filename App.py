47cd3f5a5e28065dd33dd549417b6a4e3f392463
ZMizgalski <zbigniew.mizgalskifullstack@gmail.com>
2022-04-20 15:10:51 +0200

    update

--- App.py
+++ App.py
@@ -1,9 +1,11 @@
+import io
 import mimetypes
 import os
 import json
-from flask import Flask, Response, request, flash, redirect, send_from_directory
+from flask import Flask, Response, request, flash, redirect, send_from_directory, send_file
 from werkzeug.utils import secure_filename
 from flask_cors import cross_origin
+from datetime import  datetime
 import wget
 import shutil
 import ssl
@@ -13,6 +15,9 @@ app = Flask(__name__, static_url_path='', static_folder='static', template_folde
 app.secret_key = 'E20467A8B2D5F32E451E1125BE47045DE600AA22F97046263869E291C5A49A67DD47C52D990FE0053D25FD659A4E358DE10A8F9756C9066A13B71AE860728B75'
 app.config['UPLOAD_FOLDER'] = 'images'
 
+NameOfMixedFile = "MixedFile"
+secretWorkingDirectoryKey = 'Working'
+secretDoneDirectoryKey = 'Done'
 ssl._create_default_https_context = ssl._create_unverified_context
 tmpImages = 'tmpImages'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
@@ -38,23 +43,23 @@ def get_all_filenames():
     return Response(json.dumps({'images': os.listdir("./images")}), status=200, mimetype="application/json")
 
 
-@app.route('/makePublic', methods=["POST"])
-@cross_origin()
-def upload_mixed_file():
-    if 'file' not in request.files:
-        flash('No file part')
-        return Response("Key 'file' not found in request!", status=400)
-    file = request.files['file']
-    file.filename = str(uuid.uuid4()) + mimetypes.guess_extension(file.mimetype)
-    if file and allowed_file(file.filename):
-        print(request.files.getlist('file'))
-        if len(request.files.getlist('file')) > 1:
-            return Response('Too many files', status=400)
-        img = request.files.get('file')
-        img.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(img.filename)))
-        return Response('File uploaded', status=200)
-    else:
-        return Response("No files found!", status=400)
+# @app.route('/makePublic', methods=["POST"])
+# @cross_origin()
+# def upload_mixed_file():
+#     if 'file' not in request.files:
+#         flash('No file part')
+#         return Response("Key 'file' not found in request!", status=400)
+#     file = request.files['file']
+#     file.filename = str(uuid.uuid4()) + mimetypes.guess_extension(file.mimetype)
+#     if file and allowed_file(file.filename):
+#         print(request.files.getlist('file'))
+#         if len(request.files.getlist('file')) > 1:
+#             return Response('Too many files', status=400)
+#         img = request.files.get('file')
+#         img.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(img.filename)))
+#         return Response('File uploaded', status=200)
+#     else:
+#         return Response("No files found!", status=400)
 
 
 @app.route('/getData', methods=["GET"])
@@ -69,13 +74,19 @@ def downloadImage(URL, Path):
     return wget.download(URL, Path)
 
 
+def makeDir(path):
+    isExist = os.path.exists(path)
+    if not isExist:
+        os.makedirs(path)
+
+
 def clear_directory(Path):
     for filename in os.listdir(Path):
         file_path = os.path.join(Path, filename)
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
-            elif os.path.isdir(file_path):
+            elif os.path.isdir(file_path) & (secretDoneDirectoryKey in filename):
                 shutil.rmtree(file_path)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
@@ -95,15 +106,26 @@ def renderImage():
         flash('No file part')
         return Response("Key 'secondImage' not found in request!", status=400)
 
+    makeDir(app.config['UPLOAD_FOLDER'])
+    makeDir(tmpImages)
     clear_directory(tmpImages)
+    date = datetime.now().strftime("%Y%m%d-%H%M%S")
+    path = tmpImages + '\\' + date + "_" + secretWorkingDirectoryKey
+    makeDir(path)
+
     firstExtension = firstImage.split("/")[7].split('.')[1]
     secondExtension = secondImage.split("/")[7].split('.')[1]
-    firstFile = '1.' + firstExtension
-    secondFile = '2.' + secondExtension
-    downloadImage(firstImage, tmpImages + "\\" + firstFile)
-    downloadImage(firstImage, tmpImages + "\\" + secondFile)
+    uuid2 = str(uuid.uuid4())
+    firstFile = uuid2 + '_1_.' + firstExtension
+    secondFile = uuid2 + '_2_.' + secondExtension
 
-    return send_from_directory(tmpImages, firstFile, as_attachment=True)
+    downloadImage(firstImage, path + '\\' + firstFile)
+    downloadImage(firstImage, path + '\\' + secondFile)
+
+    mixedFileName = uuid2 + "_" + NameOfMixedFile + "." + firstExtension
+    shutil.copy(path + "\\" + firstFile, path + "\\" + mixedFileName)
+    os.rename(path, tmpImages + '\\' + date + "_" + secretDoneDirectoryKey)
+    return send_from_directory(tmpImages + '\\' + date + "_" + secretDoneDirectoryKey, mixedFileName, as_attachment=True)
     # file = request.files['file']
     # if file.filename == '':
     #     flash('No selected file')
